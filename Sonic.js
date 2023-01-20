916de5f130389dd202651380c15a87b9b8827102
DouglasJohnston75 <51175848+DouglasJohnston75@users.noreply.github.com>
2023-01-20 14:08:01 -0800

    fixed animation a little

--- Sonic.js
+++ Sonic.js
@@ -1,16 +1,27 @@
 class Sonic {
 
-    constructor() {
-        this.asset = new AssetManager()
-        this.sheet = 'SonicSheet.png'
-        this.anim = new Animator(sheet, 10, 10, 10, 10, 10, 10, 10, false, true)
-    }
+    constructor(game) {
+        this.game = game;
+        // (spritesheet, xStart, yStart, width, height, frameCount, frameDuration, framePadding, reverse, loop)
+        this.animator = new Animator(ASSET_MANAGER.getAsset("./SonicSheet.png"), 0, 39, 29.1, 39, 7, 0.4, 1, false, true);
+    };
     
     update() {
+        console.log(this.animator.currentFrame())
+        var tapping = false
+        // if(this.animator.currentFrame() == 6) {
+        //     this.animator = new Animator(ASSET_MANAGER.getAsset("./SonicSheet.png"), 118, 39, 29, 39, 3, 0.2, 0, false, true)
+        //     tapping = true
+        // }
 
-    }
+        // if(tapping && this.animator.currentFrame() == 2) {
+        //     this.animator = new Animator(ASSET_MANAGER.getAsset("./SonicSheet.png"), 118, 39, 29, 39, 3, 0.2, 0, true, true)
 
-    draw() {
+        // }
 
-    }
+    };
+
+    draw(ctx) {
+        this.animator.drawFrame(this.game.clockTick, ctx, 300, 200, 5);
+    };
 }
