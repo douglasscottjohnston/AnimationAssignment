466e7d62a1090e95c80fa3d4508fee330ab936c5
DouglasJohnston75 <51175848+DouglasJohnston75@users.noreply.github.com>
2023-01-19 16:37:40 -0800

    added animator class

--- Sonic.js
+++ Sonic.js
@@ -1,10 +1,16 @@
 class Sonic {
+
+    constructor() {
+        this.asset = new AssetManager()
+        this.sheet = 'SonicSheet.png'
+        this.anim = new Animator(sheet, 10, 10, 10, 10, 10, 10, 10, false, true)
+    }
     
     update() {
 
     }
 
     draw() {
-        
+
     }
 }
